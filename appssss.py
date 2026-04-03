9ad39fead2826ab51d16258cae300f0f5a57b487
Nithiya-12 <srin02456@gmail.com>
2026-04-03 10:16:07 +0530

    Update appssss.py

--- appssss.py
+++ appssss.py
@@ -5,10 +5,22 @@ st.title("📩 Spam Detection App")
 msg = st.text_input("Enter your message")
 
 if st.button("Check"):
-    msg = msg.lower()
-    spam_words = ["offer", "win", "free", "money", "click"]
-
-    if any(word in msg for word in spam_words):
-        st.error("🚨 Spam Message")
+    if msg.strip() == "":
+        st.warning("⚠️ Please enter a message")
     else:
-        st.success("✅ Not Spam")
+        msg = msg.lower()
+        spam_words = ["offer", "win", "free", "money", "click"]
+
+        if any(word in msg for word in spam_words):
+            st.error("🚨 Spam Message")
+
+            # Play spam sound
+            audio_file = open("spam.mp3", "rb")
+            st.audio(audio_file.read(), format="audio/mp3")
+
+        else:
+            st.success("✅ Not Spam")
+
+            # Play success sound
+            audio_file = open("notspam.mp3", "rb")
+            st.audio(audio_file.read(), format="audio/mp3")
